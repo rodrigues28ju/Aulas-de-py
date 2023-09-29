ab3442f73eba0b8b422d3e85d876c527c92b942b
Juliana Rodrigues Nogueira <114073784+rodrigues28ju@users.noreply.github.com>
2023-09-29 11:43:50 -0300

    Update Repeticaowhile.py
    correçao

--- Repeticaowhile.py
+++ Repeticaowhile.py
@@ -1,5 +1,9 @@
 i = 1
+resp = "s"
 m = int(input("digite o valor da tabuada: "))
+while resp== "s:
 while i < 10: # enquanto i for menor que 10 faça
     print(i , "x ", m, " =" , i * m)
     i = i +1
+resp = input("Quer continuar? s/n")
+i=1
