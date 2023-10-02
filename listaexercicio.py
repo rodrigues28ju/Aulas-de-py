27d7be9b26a063aee6523a5d8ffe9aa56ebfbc6d
Juliana Rodrigues Nogueira <114073784+rodrigues28ju@users.noreply.github.com>
2023-10-02 10:38:53 -0300

    Add files via upload
    aula 04  02/10/2023

---
+++ listaexercicio.py
@@ -0,0 +1,28 @@
+"""
+a1 = input("Digite o nome: ")
+a2 = input("Digite o nome: ")
+a3 = input("Digite o nome: ")
+a4 = input("Digite o nome: ")
+a5 = input("Digite o nome: ")
+"""
+"""
+i = 1
+i = int(input("Quantos clientes?"))
+j = 0
+titular = []
+while j <= i:
+    titular[j] = input("Nome Cliente:  ")
+    j = j+1
+"""
+lista = ["ana","joao","maria"]
+existe = "ana" in lista # metodo de buscar algo
+print(existe)
+
+bairro =["Santa cruz","botafogo","gloria"]
+existe = input("Digite o bairro: ")
+resultado = existe in bairro
+print(resultado)
+if resultado:
+    print("Bairro cadastrado")
+else:
+    print("Bairro nao cadastrado")
