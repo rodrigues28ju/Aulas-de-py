795ad5379ff309ab3b88ad56baaf6bd13642f16e
Juliana Rodrigues Nogueira <114073784+rodrigues28ju@users.noreply.github.com>
2023-10-04 09:36:26 -0300

    Add files via upload
    aula do dia 4/10

---
+++ for.py
@@ -0,0 +1,14 @@
+frutas = ["banana", "maça", "abacate", "limao", "laranja"]
+for fruta in frutas:
+    print(fruta)
+
+for x in frutas:
+    print(x)
+    if x in frutas == "abacate":
+        break
+
+"""for letras in "curso python":
+    print(letras)
+"""
+for i in range(6):
+    print(i)
---
+++ function.py
@@ -0,0 +1,20 @@
+def minha_function():
+    print("Olá Mundo")
+
+
+minha_function()
+
+
+def minha_fuction2(nome):
+    print(nome + " Rodrigues ")
+
+
+minha_fuction2(" Juliana ")
+
+
+def sacar(valor):
+    saldo = 1000 - valor
+    print(saldo)
+
+
+sacar(36)
