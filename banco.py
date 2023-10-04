6ce8e237746208cac8de08d6d3be088231f1dfc0
Juliana Rodrigues Nogueira <114073784+rodrigues28ju@users.noreply.github.com>
2023-10-04 10:40:16 -0300

    Add files via upload
    aula dia 04/10 - continuidade

--- banco.py
+++ banco.py
@@ -5,6 +5,17 @@ class Conta:
 
     def consultar(self):
         print("Titular:  ", self.titular)
+        print("Saldo:  ", self.saldo)
+
+    def depositar(self):
+        valor = float(input("Informe o valor"))
+        self.saldo = self.saldo + valor
+
+    def sacar(self):
+        valor = float(input("Informe o valor"))
+        self.saldo = self.saldo - valor
+
+
 
 
 c1 = Conta("Maria", 1000)
@@ -12,6 +23,10 @@ c2 = Conta("Juliana", 800)
 c3 = Conta("Carlos", 900)
 c4 = Conta("Tania", 1000)
 print(c1.titular, c1.saldo)
+c1.consultar()
+c1.depositar()
+c1.consultar()
+c1.sacar()
 c1.consultar()
 """
 caso nao aceita o contrutor criar um vazio tipo c4 = Conta()
